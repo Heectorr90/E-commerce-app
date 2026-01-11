e4ae04898f10bc3828a6d7d4eceb77279da437a2
Hector Ramirez <85043574+Heectorr90@users.noreply.github.com>
2026-01-10 22:38:37 -0600

    estructura base del ecommerce fullstack

---
+++ ecommerce/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+declare var $: any;
+declare function HOMEINIT([]): any;
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'ecommerce';
+  constructor() {
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+  }
+}
